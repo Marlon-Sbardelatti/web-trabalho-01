5979517c941c334184ec55cdf29e73d041561e9a
HetzWGA <hetzwga@pop-os.localdomain>
2024-03-19 16:57:50 -0300

    treinos page and js

--- src/app.js
+++ src/app.js
@@ -3,27 +3,6 @@ let treinos_form = document.getElementById("treinos_form");
 let formButton = document.getElementById("form_button");
 
 function create() {
-  // let form = document.createElement("form");
-  // form.className = "treinos_form";
-  // let input_nome = document.createElement("input");
-  // let input_rep = document.createElement("input");
-  // let input_ser = document.createElement("input");
-  // input_rep.type = "number";
-  // input_ser.type = "number";
-
-  // input_nome.placeholder = "Nome do Exercício";
-  // input_rep.placeholder = "Repetições";
-  // input_ser.placeholder = "Séries";
-
-  // input_nome.className = "input_treinos";
-  // input_ser.className = "input_treinos";
-  // input_rep.className = "input_treinos";
-
-  // form.appendChild(input_nome);
-  // form.appendChild(input_rep);
-  // form.appendChild(input_ser);
-  // treinos.appendChild(form);
-
   let input_nome = document.createElement("input");
   let input_rep = document.createElement("input");
   let input_ser = document.createElement("input");
@@ -45,9 +24,20 @@ function create() {
   treinos_form.appendChild(input_ser);
 }
 
-function save() {
+function save(event) {
+  let inputs = [];
+
+  for (let i = 0; i < treinos_form.elements.length; i++) {
+    const element = treinos_form.elements[i];
+    console.log(element.value);
+    if (element.value == null || element.value == "") {
+      event.preventDefault();
+    }
+    inputs.push(element.value);
+  }
+  console.log(inputs);
   console.log("saved");
-  location.reload(true);
+  // location.reload(true);
 }
 
 function save_user(event) {
@@ -55,10 +45,14 @@ function save_user(event) {
   let password = document.forms[0].elements[1].value;
 
   if (user == "" || user == null || user == undefined) {
-    alert("Prencha o usuário.");
-    event.preventDefault();
-  }
-  if (password == "" || password == null || password == undefined) {
+    if (password == "" || password == null || password == undefined) {
+      alert("Prencha todos os campos.");
+      event.preventDefault();
+    } else {
+      alert("Prencha o usuário.");
+      event.preventDefault();
+    }
+  } else if (password == "" || password == null || password == undefined) {
     alert("Prencha a senha.");
     event.preventDefault();
   }
@@ -76,4 +70,8 @@ function showPassword() {
   } else {
     pswInput.type = "password";
   }
+}
+
+function editTable() {
+  console.log("Edit it");
 }
