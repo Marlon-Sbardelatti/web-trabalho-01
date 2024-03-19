16b780c5a575bd45df00a8ebf8f0e747166e6156
Marlon-Sbardelatti <hetzwga@gmail.com>
2024-03-19 20:42:25 -0300

    saving treinos in localstorage

--- src/app.js
+++ src/app.js
@@ -27,17 +27,58 @@ function create() {
 function save(event) {
   let inputs = [];
 
-  for (let i = 0; i < treinos_form.elements.length; i++) {
-    const element = treinos_form.elements[i];
-    console.log(element.value);
+  for (let i = 0; i < treinos.getElementsByTagName("input").length; i++) {
+    const element = treinos.getElementsByTagName("input")[i];
+    // console.log(element);
+    // console.log(element.value);
     if (element.value == null || element.value == "") {
       event.preventDefault();
     }
     inputs.push(element.value);
   }
-  console.log(inputs);
-  console.log("saved");
-  // location.reload(true);
+  // verificando inputs
+  if (!verificarInputs(inputs)) {
+    alert("Os campos não podem estar vazios.");
+  } else {
+    let t = localStorage.getItem("treinos");
+
+    if (t == null) {
+      let treinos = [];
+      treinos.push(inputs);
+      let obj = JSON.stringify(treinos);
+      localStorage.setItem("treinos", obj);
+    } else {
+      let treinos = localStorage.getItem("treinos");
+      let treinos_obj = JSON.parse(treinos);
+      let temp_arr = [];
+
+      for (let i = 0; i < treinos_obj.length; i++) {
+        const element = treinos_obj[i];
+        temp_arr.push(element);
+      }
+
+      temp_arr.push(inputs);
+      temp_arr = JSON.stringify(temp_arr);
+      localStorage.setItem("treinos", temp_arr);
+    }
+
+    console.log("saved");
+    // location.reload(true);
+  }
+}
+function verificarInputs(inputs) {
+  let found = false;
+  inputs.forEach((element) => {
+    if (element == "") {
+      found = true;
+    }
+  });
+
+  if (!found) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function save_user(event) {
